e3adc391ff99120383fe511e243f285f9ddf002f
Zach Reedy <zach@codewithfeather.com>
2018-01-01 23:36:57 -0500

    Changed: GMResourceManager implements IReadOnlyDictionary and has Get methods now

--- YoYoProject/GMResourceManager.cs
+++ YoYoProject/GMResourceManager.cs
@@ -1,22 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using YoYoProject.Controllers;
 
 namespace YoYoProject
 {
-    // TODO IReadOnlyDictionary?
-    public sealed class GMResourceManager : IEnumerable<GMResource>
+    public sealed class GMResourceManager : IReadOnlyDictionary<Guid, GMResource>
     {
-        public int Count
-        {
-            get { return resources.Count; }
-        }
+        public int Count => resources.Count;
 
-        public ControllerBase this[Guid key]
-        {
-            get { return resources[key]; }
-        }
+        public IEnumerable<Guid> Keys => resources.Keys;
+
+        public IEnumerable<GMResource> Values => resources.Values;
+
+        public GMResource this[Guid key] => Get(key);
 
         private readonly SortedDictionary<Guid, GMResource> resources;
 
@@ -38,7 +37,51 @@ namespace YoYoProject
             return resource;
         }
 
-        public SortedDictionary<Guid, GMResourceInfoModel> SerializeResourceInfo()
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TResource Get<TResource>(Guid id)
+            where TResource : GMResource, new()
+        {
+            return (TResource)Get(id);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public GMResource Get(Guid id)
+        {
+            return resources[id];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TResource Get<TResource>(string name)
+            where TResource : GMResource, new()
+        {
+            return (TResource)Get(name);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public GMResource Get(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            return resources.Values.Single(x => x.Name == name);
+        }
+
+        public bool ContainsKey(Guid key)
+        {
+            return resources.ContainsKey(key);
+        }
+
+        public bool TryGetValue(Guid key, out GMResource value)
+        {
+            return resources.TryGetValue(key, out value);
+        }
+
+        internal SortedDictionary<Guid, GMResourceInfoModel> SerializeResourceInfo()
         {
             var resourceInfos = new SortedDictionary<Guid, GMResourceInfoModel>();
             foreach (var kvp in resources)
@@ -48,10 +91,10 @@ namespace YoYoProject
 
             return resourceInfos;
         }
-
-        public IEnumerator<GMResource> GetEnumerator()
+        
+        public IEnumerator<KeyValuePair<Guid, GMResource>> GetEnumerator()
         {
-            return resources.Values.GetEnumerator();
+            return resources.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
