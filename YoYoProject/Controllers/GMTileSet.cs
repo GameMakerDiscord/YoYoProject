6241e874479a7f8b5596ddf3831b4d89d93e1781
Zach Reedy <zach@codewithfeather.com>
2018-01-17 00:02:51 -0500

    WIP: GMTileSet

--- YoYoProject/Controllers/GMTileSet.cs
+++ YoYoProject/Controllers/GMTileSet.cs
@@ -97,13 +97,24 @@ namespace YoYoProject.Controllers
             get { return GetProperty(animation); }
             set { SetProperty(value, ref animation); }
         }
+
+        public int TileCount
+        {
+            get
+            {
+                if (Sprite == null)
+                    return 0;
+
+                return (Sprite.Width / TileWidth) * (Sprite.Height / TileHeight);
+            }
+        }
         
         protected internal override string ResourcePath => $@"tilesets\{Name}\{Name}.yy";
 
         public GMTileSet()
         {
             AutoTileSets = new List<GMAutoTileSet>();
-            Animation = new GMTileSetAnimation();
+            Animation = new GMTileSetAnimation(this);
         }
 
         protected internal override void Create()
@@ -121,13 +132,34 @@ namespace YoYoProject.Controllers
             OutTileBorderVertical = 2;
         }
 
+        internal int GetTileIndex(int x, int y)
+        {
+            if (Sprite == null)
+                return 0;
+
+            int stride = Sprite.Width / TileWidth;
+            x = Math.Min(Math.Max(x, 0), Sprite.Width / TileWidth);
+            y = Math.Min(Math.Max(y, 0), Sprite.Height / TileHeight);
+
+            return y * stride + x;
+        }
+
         protected internal override ModelBase Serialize()
         {
+            int outColumns = 1;
+            int tileCount = 0;
+
+            if (Sprite != null)
+            {
+                outColumns = Sprite.Width / (OutTileBorderHorizontal * 2 + TileWidth);
+                tileCount = TileCount;
+            }
+
             return new GMTileSetModel
             {
                 id = Id,
                 name = Name,
-                out_columns = 0, // TODO IMPLEMENT
+                out_columns = outColumns,
                 spriteId = Sprite?.Id ?? Guid.Empty,
                 tilewidth = TileWidth,
                 tileheight = TileHeight,
@@ -139,9 +171,9 @@ namespace YoYoProject.Controllers
                 textureGroupId = TextureGroup?.Id ?? Guid.Empty,
                 out_tilehborder = OutTileBorderHorizontal,
                 out_tilevborder = OutTileBorderVertical,
-                tile_count = 0, // TODO IMPLEMENT
+                tile_count = tileCount,
                 auto_tile_set = AutoTileSets.Select(x => (GMAutoTileSetModel)x.Serialize()).ToList(),
-                tile_animation_frames = Animation.Frames
+                tile_animation_frames = Animation.Animations
                                                  .Select(x => (GMTileAnimationFrameModel)x.Serialize())
                                                  .ToList(),
                 tile_animation_speed = Animation.Speed,
@@ -151,39 +183,107 @@ namespace YoYoProject.Controllers
         }
     }
 
+    // TODO Implement some API to make this actually usable...
     public sealed class GMAutoTileSet : ControllerBase
     {
+        private string name;
+        public string Name
+        {
+            get { return GetProperty(name); }
+            set { SetProperty(value, ref name); }
+        }
+        
+        private List<uint> tiles;
+        public List<uint> Tiles
+        {
+            get { return GetProperty(tiles); }
+            set { SetProperty(value, ref tiles); }
+        }
+        
+        private bool closedEdge;
+        public bool ClosedEdge
+        {
+            get { return GetProperty(closedEdge); }
+            set { SetProperty(value, ref closedEdge); }
+        }
+
+        internal GMAutoTileSet()
+        {
+            Tiles = new List<uint>();
+        }
+
         protected internal override ModelBase Serialize()
         {
-            throw new NotImplementedException();
+            return new GMAutoTileSetModel
+            {
+                id = Id,
+                name = Name,
+                closed_edge = ClosedEdge
+            };
         }
     }
 
     public sealed class GMTileSetAnimation : ControllerBase
     {
-        public List<Frame> Frames { get; }
+        public List<Animation> Animations { get; }
 
         public float Speed { get; set; }
 
-        internal GMTileSetAnimation()
+        private readonly GMTileSet tileSet;
+
+        internal GMTileSetAnimation(GMTileSet tileSet)
         {
-            Frames = new List<Frame>();
+            if (tileSet == null)
+                throw new ArgumentNullException(nameof(tileSet));
+
+            Animations = new List<Animation>();
+
+            this.tileSet = tileSet;
         }
 
         protected internal override ModelBase Serialize()
         {
-            throw new NotImplementedException();
+            int frameCount = Animations.Max(x => x.Frames.Count);
+            uint[] frameData = new uint[tileSet.TileCount * frameCount];
+            for (int i = 0; i < tileSet.TileCount; ++i)
+            {
+                for (int j = 0; j < frameCount; ++j)
+                    frameData[i * frameCount + j] = (uint)i; // TODO Unless overrides are found in Animation.Frames
+            }
+
+            return new GMTileAnimationModel
+            {
+                AnimationCreationOrder = null, // TODO What is this?
+                FrameData = frameData,
+                SerialiseFrameCount = frameCount
+            };
         }
 
-        public sealed class Frame : ControllerBase
+        public sealed class Animation : ControllerBase
         {
-            public string Name { get; set; }
-
-            public List<uint> frames { get; set; }
+            private string name;
+            public string Name
+            {
+                get { return GetProperty(name); }
+                set { SetProperty(value, ref name); }
+            }
+            
+            // TODO Make this usable...
+            private List<uint> frames;
+            public List<uint> Frames
+            {
+                get { return GetProperty(frames); }
+                set { SetProperty(value, ref frames); }
+            }
 
             protected internal override ModelBase Serialize()
             {
-                throw new NotImplementedException();
+                return new GMTileAnimationFrameModel
+                {
+                    id = Id,
+                    name = Name,
+                    frames = Frames
+                };
             }
         }
     }
